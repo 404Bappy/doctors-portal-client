8cdf856483aa81d340cfbed6994138e2af68be10
Bappy71 <19103076@iubat.edu>
2023-01-04 19:19:18 +0600

    Initial Commit

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import InfoCard from './InfoCard';
 import clock from '../../assets/icons/clock.svg';
+import marker from '../../assets/icons/marker.svg';
+import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
+
     return (
-        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5'>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
-            <InfoCard img={clock}></InfoCard>
+        <div className='grid grid-cols-1 lg:grid-cols-3 gap-5 px-12'>
+            <InfoCard cardTitle="Opening Hours" element="Opening ( 10 AM – 6 PM )" bgClass="bg-gradient-to-r from-secondary to-primary" img={clock}></InfoCard>
+            <InfoCard cardTitle="Our Locations" element="Sector 10 Uttara, 12/73" bgClass="bg-accent" img={marker}></InfoCard>
+            <InfoCard cardTitle="Contact Us" element="01849675876" bgClass="bg-gradient-to-r from-secondary to-primary" img={phone}></InfoCard>
         </div>
     );
 };
