ce60116107a644791222df6b2e9d808194a980a2
Bappy71 <19103076@iubat.edu>
2023-01-06 15:01:41 +0600

    Initial Commit

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,20 +1,38 @@
 import React from 'react';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithEmailAndPassword, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
 import { useForm } from "react-hook-form";
 
 const Login = () => {
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, gUser, gLoading, gError] = useSignInWithGoogle(auth);
     const { register, formState: { errors }, handleSubmit } = useForm();
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+
+    let signInError;
+
+    if (loading || gLoading) {
+        return <button class="btn loading">loading</button>
+    }
+
+    if (error || gError) {
+        signInError = <p>{error?.message || gError?.message}</p>
+    }
 
 
-    if (user) {
-        console.log(user);
+    if (gUser) {
+        console.log(gUser);
     }
 
     const onSubmit = data => {
         console.log(data);
+        signInWithEmailAndPassword(data.email, data.password);
     }
+
     return (
         <div className='flex h-screen justify-center items-center  '>
             <div class="card w-96 bg-base-100 shadow-xl">
@@ -78,9 +96,12 @@ const Login = () => {
                         </div>
 
 
-
+                        {signInError}
                         <input className='btn w-full max-w-xs text-white' type="submit" value="Login" />
                     </form>
+
+                    <div className='divider'>OR</div>
+
                     <button
                         onClick={() => signInWithGoogle()}
                         className="btn btn-outline"
