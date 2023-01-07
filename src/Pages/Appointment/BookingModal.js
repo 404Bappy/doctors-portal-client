80b603c4509090d1ae5d521bb6a96f2979610889
Bappy71 <19103076@iubat.edu>
2023-01-07 23:03:24 +0600

    Initial Commit

--- src/Pages/Appointment/BookingModal.js
+++ src/Pages/Appointment/BookingModal.js
@@ -2,15 +2,27 @@ import React from 'react';
 import { format } from 'date-fns';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import auth from '../../firebase.init';
+import { toast } from 'react-toastify';
 
 const BookingModal = ({ date, treatment, setTreatment }) => {
     const { _id, name, Slots } = treatment;
     const [user, loading, error] = useAuthState(auth);
-
+    const formattedDate = format(date, 'PP');
     const handleBooking = event => {
         event.preventDefault();
         const slot = event.target.slot.value;
-        console.log(_id, name, slot);
+
+
+        const booking = {
+            treatmentID: _id,
+            treatmentName: name,
+            date: formattedDate,
+            slot,
+            patient: user.email,
+            patientName: user.displayName,
+            phone: event.target.phone.value
+        }
+
         setTreatment(null);
     }
     return (
@@ -24,10 +36,10 @@ const BookingModal = ({ date, treatment, setTreatment }) => {
                         <input type="text" disabled value={format(date, 'PP')} class="input w-full max-w-xs" />
                         <select name="slot" class="select select-bordered w-full max-w-xs">
                             {
-                                Slots.map((slot, index ) => <option
+                                Slots.map((slot, index) => <option
                                     key={index}
                                     value={slot}
-                                    >{slot}</option>)
+                                >{slot}</option>)
                             }
 
                         </select>
